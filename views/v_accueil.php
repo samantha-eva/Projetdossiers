e147dc799575b051188d6396d98e8779f2c98f9b
samantha-eva <samanthathiebaut@gmail.com>
2019-10-04 10:25:47 +0200

    cc

---
+++ views/v_accueil.php
@@ -0,0 +1,47 @@
+<html>
+<body>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+
+<nav class="nav bg-dark">
+  <a class="nav-link active">Profil</a>
+  <a class="nav-link" href="#">valider fiche de frais </a>
+  <a class="nav-link" href="#">Suivis des paiements</a>
+</nav>
+
+            
+<div class="contenu">
+
+    <h2>Bienvenue sur l'application GSB APPLI FRAIS - Espace comptable</h2>
+
+    <div class="contenu_interne">
+
+        <p>
+
+           Cette application vous permet :
+
+            <ul>
+
+                <li>De contrôler et valider les fiches de frais des visiteurs dans la partie "Valider fiche de frais"</li>
+
+                <li>De mettre en paiement et de consulter l'historique des fiches de frais des visiteurs dans la partie "Suivre paiement fiche de frais"</li> 
+
+            </ul>
+
+        </p>
+
+    </div>
+
+</div>
+
+
+    
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		</body>
+</html>
