bbdb138155f1509e33ea97fd386c6a493524d338
samantha-eva <samanthathiebaut@gmail.com>
2019-11-04 11:22:57 +0100

    ajout index

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require_once("include/fct.inc.php");
+require_once ("include/class.pdogsb.inc.php");
+include("vues/v_entete.php") ;
+session_start();
+$pdo = PdoGsb::getPdoGsb();
+$estConnecte = estConnecte();
+if(!isset($_REQUEST['uc']) || !$estConnecte){
+     $_REQUEST['uc'] = 'connexion';
+}	 
+$uc = $_REQUEST['uc'];
+switch($uc){
+	case 'connexion':{
+		include("controleurs/c_connexion.php");break;
+	}
+
+	case'validerFicheFrais':{
+		include("controleurs/c_validerFicheFrais.php");break;
+	}
+	case'afficherFicheFrais':{
+		include("controleurs/c_afficherFicheFrais.php");break;
+	}
+	case'suivisFicheFrais':{
+		include("controleurs/c_suivisFicheFrais.php");break;
+	}
+
+}
+include("vues/v_pied.php") ;
+
+?>
